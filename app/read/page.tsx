cf8aa2fd2a1a10c7e24cee2f0a511ff30aadb69f
ColossalRenders <144396943+ColossalRenders@users.noreply.github.com>
2024-11-15 13:44:49 -0800

    Update page.tsx

--- app/read/page.tsx
+++ app/read/page.tsx
@@ -1,8 +1,26 @@
+import { allPosts } from "@/.contentlayer/generated"
+import Link from "next/link"
+
 export default function ReadPage() {
   return (
-    <div>
-      <h1>Read</h1>
-      <p>This is the Read page.</p>
-    </div>
+    <>
+      <div>
+        <h1>Read</h1>
+        <p>This is the Read page.</p>
+      </div>
+    
+      <div className="prose dark:prose-invert">
+        {allPosts
+          .filter((post) => post.type === "text")
+          .map((post) => (
+          <article key={post._id}>
+            <Link href={post.slug}>
+              <h2>{post.title}</h2>
+            </Link>
+            {post.description && <p>{post.description}</p>}
+          </article>
+        ))}
+      </div>
+    </>
   )
 }
