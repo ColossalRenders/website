afd6ffa90006aa9af971e1e1071b77a2a9638adb
ColossalRenders <144396943+ColossalRenders@users.noreply.github.com>
2024-11-14 16:40:43 -0800

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -3,25 +3,27 @@ import Link from "next/link"
 import Image from 'next/image'
 
 export default function Home() {
-  return [
-    <div>
-      <Image
-        src="/blog-post-4.jpg"
-        width={500}
-        height={500}
-        alt="Picture of the author"
-      />
-    </div>,
+  return (
+    <>
+      <div>
+        <Image
+          src="/blog-post-4.jpg"
+          width={500}
+          height={500}
+          alt="Picture of the author"
+        />
+      </div>
     
-    <div className="prose dark:prose-invert">
-      {allPosts.map((post) => (
-        <article key={post._id}>
-          <Link href={post.slug}>
-            <h2>{post.title}</h2>
-          </Link>
-          {post.description && <p>{post.description}</p>}
-        </article>
-      ))}
-    </div>
-  ]
+      <div className="prose dark:prose-invert">
+        {allPosts.map((post) => (
+          <article key={post._id}>
+            <Link href={post.slug}>
+              <h2>{post.title}</h2>
+            </Link>
+            {post.description && <p>{post.description}</p>}
+          </article>
+        ))}
+      </div>
+    </>
+  )
 }
