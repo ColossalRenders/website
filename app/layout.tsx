1efc6cdf08cad7b37708179a83687cb7160533c9
ColossalRenders <144396943+ColossalRenders@users.noreply.github.com>
2024-11-24 08:37:05 +0000

    reset

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,8 +2,7 @@ import Link from "next/link"
 import "./globals.css"
 import { Inter } from "next/font/google"
 import { Noto_Sans } from "next/font/google"
-import { GeistSans } from 'geist/font/sans'
-import { GeistMono } from 'geist/font/mono'
+import { GeistSans } from "geist/font/sans"
 import { ThemeProvider } from "@/components/theme-provider"
 import { Analytics } from "@/components/analytics"
 import { ModeToggle } from "@/components/mode-toggle"
@@ -27,7 +26,7 @@ export default function RootLayout({ children }: RootLayoutProps) {
   return (
     <html lang="en">
       <body
-        className={`antialiased min-h-screen bg-white dark:bg-slate-950 text-slate-900 dark:text-slate-50 ${GeistSans.variable} ${GeistMono.variable}`}
+        className={`antialiased min-h-screen bg-white dark:bg-slate-950 text-slate-900 dark:text-slate-50 ${GeistSans.variable}`}
         //style={{ fontFeatureSettings: "'ss04' on" }} // Enable stylistic set ss04
       >
         <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
