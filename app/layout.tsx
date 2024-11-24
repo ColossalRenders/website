f22a479c5280edf0d01cd4d3ad99fa14dcc632d7
ColossalRenders <144396943+ColossalRenders@users.noreply.github.com>
2024-11-24 10:55:25 +0000

    bruh

--- app/layout.tsx
+++ app/layout.tsx
@@ -31,7 +31,7 @@ export default function RootLayout({ children }: RootLayoutProps) {
         //style={{ fontFeatureSettings: "'ss04' on" }} // Enable stylistic set ss04
       >
         <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
-        <div className="w-screen py-10 px-4">
+        <div className="max-w-2xl mx-auto py-10 px-4">
             <header>
               <div className="flex items-center justify-between">
                 <ModeToggle />
