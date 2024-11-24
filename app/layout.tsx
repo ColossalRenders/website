5fe28ca7e9711f2ba97aa573003cc15f7304fd7f
ColossalRenders <144396943+ColossalRenders@users.noreply.github.com>
2024-11-23 23:39:06 -0800

    use Geist

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,12 +2,17 @@ import Link from "next/link"
 import "./globals.css"
 import { Inter } from "next/font/google"
 import { Noto_Sans } from "next/font/google"
+import { Geist } from "next/font/google"
 import { ThemeProvider } from "@/components/theme-provider"
 import { Analytics } from "@/components/analytics"
 import { ModeToggle } from "@/components/mode-toggle"
 
 const inter = Inter({ subsets: ["latin"] })
 const notoSans = Noto_Sans({ 
+  subsets: ["latin"], 
+  weight: ["400", "700"], 
+ })
+const geist = Geist({ 
   subsets: ["latin"], 
   weight: ["400", "700"], 
  })
@@ -25,8 +30,8 @@ export default function RootLayout({ children }: RootLayoutProps) {
   return (
     <html lang="en">
       <body
-        className={`antialiased min-h-screen bg-white dark:bg-slate-950 text-slate-900 dark:text-slate-50 ${inter.className}`}
-        style={{ fontFeatureSettings: "'ss04' on" }} // Enable stylistic set ss04
+        className={`antialiased min-h-screen bg-white dark:bg-slate-950 text-slate-900 dark:text-slate-50 ${geist.className}`}
+        //style={{ fontFeatureSettings: "'ss04' on" }} // Enable stylistic set ss04
       >
         <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
           <div className="max-w-2xl mx-auto py-10 px-4">
