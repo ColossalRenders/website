a47c5ee43854852822ac683693fb8b7140e37ec4
ColossalRenders <144396943+ColossalRenders@users.noreply.github.com>
2024-12-05 16:09:03 -0800

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -31,7 +31,8 @@ export default function RootLayout({ children }: RootLayoutProps) {
         //style={{ fontFeatureSettings: "'ss04' on" }} // Enable stylistic set ss04
       >
         <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
-        <div className="max-w-screen-2xl mx-auto py-10 px-4">
+          {/* Main centered container */}
+          <div className="container mx-auto py-10 px-4">
             <header>
               <div className="flex items-center justify-between">
                 <ModeToggle />
@@ -50,5 +51,5 @@ export default function RootLayout({ children }: RootLayoutProps) {
         </ThemeProvider>
       </body>
     </html>
-  )
+  );
 }
